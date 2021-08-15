84975f52f8991e00bb3ed50ed0eff688f8b78356
GSEmanuel <58973884+GSEmanuel@users.noreply.github.com>
2021-08-15 00:28:49 -0300

    como hinata, la rama principal
    mi proyecto base para empezar a crear mi calculadora cientifica, voy a hacerla de a poquito, primero ponerle cosas faciles y luego ir avanzando con la complejidad

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.config(bg= "#282923")
+root.title("calculadora")
+root.resizable(False, False)
+
+
+
+root.mainloop()
+
+#pd: te amo
